a1e634a480d006da480a2749b5affbeed88e831b
André Luiz da Silva <111994982+andrejava2022@users.noreply.github.com>
2023-07-28 15:23:33 -0300

    Desafio concluido com sucesso

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("551156779338", "C1", "12345678910", 64 );
+Smartphone iphone = new Iphone("551155649338", "I12", "5698778910", 512 );
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("DIO");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("DIO");
